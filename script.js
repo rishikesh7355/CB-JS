22744b4e9b054a2347b22e36310628671e486ed9
Rishikesh <rishiyadav280198@gmail.com>
2020-08-25 16:04:58 +0530

    newrp

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello World")
+let a=10;
+let result=""
+for(let i=0;i<a;i++){
+result+= i +" ";
+}
+
+    console.log(result)
---
+++ syntax.js
@@ -0,0 +1,15 @@
+// single line comment
+/*multi
+linr commment */
+
+var a=10 //semicolon colon can be used as we wish
+var b=10;
+
+
+let c=90;
+const e=40;
+c++;
+console.log(c)
+//e++;// not possble as e is const
+
+
