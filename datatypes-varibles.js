5f59455e50089ac832700c1509d3b03f6aebe694
Rishikesh <rishiyadav280198@gmail.com>
2020-08-26 21:53:42 +0530

    cstnse

---
+++ datatypes-varibles.js
@@ -0,0 +1,6 @@
+let num=1;
+let string ='this is string js';
+let bool= true;
+console.log(typeof num)
+ num ='one'
+console.log(typeof num)//varible name can contain anytype of data so cannot say what data is  stored inside variable exp num here//
---
+++ object.js
@@ -0,0 +1,7 @@
+let object ={
+    a:10,
+    b:'hello',
+    c:true,
+
+}
+console.log(obj.a)
