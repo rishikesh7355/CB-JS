94f5aa51003580825aad1e79d9d627062a0a565e
Rishikesh <rishiyadav280198@gmail.com>
2020-08-27 09:05:00 +0530

    objects

--- object.js
+++ object.js
@@ -1,7 +1,21 @@
-let object ={
+let obj ={
     a:10,
     b:'hello',
     c:true,
+    d:{
+        x:'ri',
+        y:'shi',
+        z:{
+            w:'yadav',
+        }
+
+    }
 
 }
 console.log(obj.a)
+console.log(obj.d.z.w)
+adding new item see next line
+obj.e = 11111;
+console.log(obj)
+delete Object.e//remove properties
+console.log(obj)
