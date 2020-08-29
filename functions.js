2e904119383dc0bf35017c55a54d20029fc669a6
Rishikesh <rishiyadav280198@gmail.com>
2020-08-29 22:10:30 +0530

    git commit functions.js -m

---
+++ functions.js
@@ -0,0 +1,19 @@
+function incr(x)//no need to declare type of varible like in c++etc
+{
+    return(x+1)
+}
+let five=incr(4)
+console.log(five)
+
+let next=incr(-9)
+console.log(next)
+
+let t=incr(true)//trueis actually =1 which is typecated automatically
+console.log(t)
+
+let str=incr('rishikesh')//rishikesh+1=rishieksh1 one is typecated into string
+console.log(str)
+
+
+
+
